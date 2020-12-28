19a2a5bbe3e328f7647f5112a9abd7cdafbb4d04
Andreas Nilsson <andreas.nilsson@jayway.com>
2020-12-28 19:50:58 +0100

    remove nil

--- src/alang/env.hpp
+++ src/alang/env.hpp
@@ -24,36 +24,22 @@ namespace alang {
   inline void set_prim(Env&  env, string id, Prim::Imp imp) { set(env, id, types::Prim, id, imp); }
 
   inline void init(lgpp::Env& env) {
+    set_meta(env, types::Any);
     set_meta(env, types::Coro);
     set_meta(env, types::Int);
-    set_meta(env, types::Nil);
     set_meta(env, types::Macro);
     set_meta(env, types::Meta);
     set_meta(env, types::Num);
     set_meta(env, types::Pair);
+    set_meta(env, types::Prim);
     set_meta(env, types::Seq);
     set_meta(env, types::Stack);
     set_meta(env, types::Thread);
 
     set_macro(env, "_", [](Parser& in, Thread &out, Env& env) {});
-
-    set_macro(env, ".isa", [](Parser& in, Thread &out, Env& env) {
-      auto parent = pop(in);
-      compile(parent, in, out, env);
-      emit<ops::Isa>(out);
-    });
-
     set_macro(env, "cp", [](Parser& in, Thread &out, Env& env) { emit<ops::Cp>(out); });
     set_macro(env, "d", [](Parser& in, Thread &out, Env& env) { emit<ops::Drop>(out); });
 
-    set_macro(env, "go", [](Parser& in, Thread &out, Env& env) {
-      auto idt = pop(in);
-      auto id = idt.as<toks::Id>().name;
-      auto target = find(env, id);
-      if (!target) { throw EParse(idt.pos, "Unknown label: ", id); }
-      emit<ops::Go>(out, *target->as(types::Label));
-    });
-    
     set_macro(env, "if", [](Parser& in, Thread &out, Env& env) {
       Tok cond = pop(in), x = pop(in), y = pop(in);
       compile(cond, in, out, env);
@@ -65,6 +51,12 @@ namespace alang {
       xskip.pc = emit_pc(out);
       compile(y, in, out, env);
       yskip.pc = emit_pc(out);
+    });
+
+    set_macro(env, ".isa", [](Parser& in, Thread &out, Env& env) {
+      auto parent = pop(in);
+      compile(parent, in, out, env);
+      emit<ops::Isa>(out);
     });
 
     set_macro(env, "label", [](Parser& in, Thread &out, Env& env) {
@@ -92,7 +84,6 @@ namespace alang {
       push(s, types::Bool, pop(s) > r);
     });
 
-    set(env, "NA", types::Nil, nullptr);
     set(env, "T", types::Bool, true);
     set(env, "F", types::Bool, false);
   }
