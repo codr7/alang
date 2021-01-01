4147ae19229174de9b6037cb1e890bf8b06a2106
Andreas Nilsson <andreas.nilsson@jayway.com>
2021-01-02 00:55:14 +0100

    extract types

--- src/alang/vm.hpp
+++ src/alang/vm.hpp
@@ -2,6 +2,15 @@
 #define ALANG_VM_HPP
 
 #include <lgpp/vm.hpp>
+#include <lgpp/types/bool.hpp>
+#include <lgpp/types/coro.hpp>
+#include <lgpp/types/int.hpp>
+#include <lgpp/types/label.hpp>
+#include <lgpp/types/macro.hpp>
+#include <lgpp/types/meta.hpp>
+#include <lgpp/types/pair.hpp>
+#include <lgpp/types/prim.hpp>
+#include <lgpp/types/stack.hpp>
 
 namespace alang {
   struct VM: lgpp::VM {
